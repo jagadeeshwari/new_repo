7421a864fcbefd73a2a049843e9c4e3fa4735228
jagadeeshwari <jagadeeshwari44@gmail.com>
2018-07-13 17:39:43 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 echo "first build"
-cd /home/ec2-user
-sudo -su ec2-user mkdir first_build1
+cd /local/Automationscripts/stage/temp
+mkdir first_build1
 cd first_build
-sudo -su ec2-user touch build1.sh
+touch build1.sh
